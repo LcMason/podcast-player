85232493f17935219cea9437b755757c3b7e4e29
Lionel <LcMason3@gmail.com>
2025-12-06 09:28:12 -0500

    creates public folder. Modifies server.js

--- server.js
+++ server.js
@@ -1,11 +1,69 @@
 import express from "express";
+import path from "path";
+import dotenv from "dotenv";
+import CryptoJS from "crypto-js";
+import fetch from "node-fetch";
+import { fileURLToPath } from "url";
+
+dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello Lionel");
+const authKey = process.env.AUTH_KEY;
+const secretKey = process.env.SECRET_KEY;
+const userAgent = process.env.USER_AGENT;
+const apiEndpoint = process.env.API_ENDPOINT;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+//  Shared authentication function
+function generateAuthHeaders() {
+    const apiHeaderTime = Math.floor(new Date().getTime() / 1000);
+    const hash = CryptoJS.SHA1(authKey + secretKey + apiHeaderTime).toString(CryptoJS.enc.Hex);
+
+    return {
+        'User-Agent': userAgent,
+        'X-Auth-Key': authKey,
+        'X-Auth-Date': apiHeaderTime.toString(),
+        'Authorization': hash
+    }
+}
+
+//  Search for podcasts
+app.get('/api/search', async (req, res) => {
+    const query = req.query.q;
+    if (!query) {
+        return res.status(400).json({ error: 'Query Parameter is required'})
+    }
+    
+    const headers = generateAuthHeaders();
+
+    try {
+        const response = await fetch(`${apiEndpoint}/search/byterm?q=${encodeURIComponent(query)}`, {
+            method: 'GET',
+            headers: headers
+        });
+
+        // if (response.ok && response.headers.get('content-type').includes('application/json')) {
+        if (response.ok) {
+            const data = await response.json();
+            res.json(data);
+        } else {
+            const rawText = await response.text();
+            console.log('Raw Response:', rawText);
+            res.status(500).json({error: 'Invalid response from API', rawText})
+        }
+    } catch (error) {
+        console.error('Error fetching API:', error.message);
+        res.status(500).json({ error: error.message});
+    }
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
+app.listen(PORT, () => {
+//   console.log("Server is running on http://localhost:3000");
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
