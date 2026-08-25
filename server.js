027653bab82776606685e672bed060f70620128a
Lionel <LcMason3@gmail.com>
2026-08-25 13:42:26 -0400

    adds Git refresher practice comment

--- server.js
+++ server.js
@@ -22,78 +22,87 @@ app.use(express.static(path.join(__dirname, "public")));
 
 //  Shared authentication function
 function generateAuthHeaders() {
-    const apiHeaderTime = Math.floor(new Date().getTime() / 1000);
-    const hash = CryptoJS.SHA1(authKey + secretKey + apiHeaderTime).toString(CryptoJS.enc.Hex);
+  const apiHeaderTime = Math.floor(new Date().getTime() / 1000);
+  const hash = CryptoJS.SHA1(authKey + secretKey + apiHeaderTime).toString(
+    CryptoJS.enc.Hex,
+  );
 
-    return {
-        'User-Agent': userAgent,
-        'X-Auth-Key': authKey,
-        'X-Auth-Date': apiHeaderTime.toString(),
-        'Authorization': hash
-    }
+  return {
+    "User-Agent": userAgent,
+    "X-Auth-Key": authKey,
+    "X-Auth-Date": apiHeaderTime.toString(),
+    Authorization: hash,
+  };
 }
 
 //  Search for podcasts
-app.get('/api/search', async (req, res) => {
-    const query = req.query.q;
-    if (!query) {
-        return res.status(400).json({ error: 'Query Parameter is required'})
-    }
-    
-    const headers = generateAuthHeaders();
+app.get("/api/search", async (req, res) => {
+  const query = req.query.q;
+  if (!query) {
+    return res.status(400).json({ error: "Query Parameter is required" });
+  }
 
-    try {
-        const response = await fetch(`${apiEndpoint}/search/byterm?q=${encodeURIComponent(query)}`, {
-            method: 'GET',
-            headers: headers
-        });
-        // if (response.ok && response.headers.get('content-type').includes('application/json')) {
-        if (response.ok) {
-            const data = await response.json();
-            res.json(data);
-        } else {
-            const rawText = await response.text();
-            console.log('Raw Response:', rawText);
-            res.status(500).json({error: 'Invalid response from API', rawText})
-        }
-    } catch (error) {
-        console.error('Error fetching API:', error.message);
-        res.status(500).json({ error: error.message});
+  const headers = generateAuthHeaders();
+
+  try {
+    const response = await fetch(
+      `${apiEndpoint}/search/byterm?q=${encodeURIComponent(query)}`,
+      {
+        method: "GET",
+        headers: headers,
+      },
+    );
+    // if (response.ok && response.headers.get('content-type').includes('application/json')) {
+    if (response.ok) {
+      const data = await response.json();
+      res.json(data);
+    } else {
+      const rawText = await response.text();
+      console.log("Raw Response:", rawText);
+      res.status(500).json({ error: "Invalid response from API", rawText });
     }
+  } catch (error) {
+    console.error("Error fetching API:", error.message);
+    res.status(500).json({ error: error.message });
+  }
 });
 
-
 //  Fetch episodes by iTunesID
-app.get('/api/episodes', async (req, res) => {
-    const feedId = req.query.feedId;
-    const max = req.query.max;
-    if (!feedId) {
-        return res.status(400).json({ error: 'Feed ID Parameter is required'})
-    }
-    
-    const headers = generateAuthHeaders();
+app.get("/api/episodes", async (req, res) => {
+  const feedId = req.query.feedId;
+  const max = req.query.max;
+  if (!feedId) {
+    return res.status(400).json({ error: "Feed ID Parameter is required" });
+  }
 
-    try {
-        const response = await fetch(`${apiEndpoint}/episodes/byitunesid?id=${encodeURIComponent(feedId)}&max=${max}`, {
-            method: 'GET',
-            headers: headers
-        });
-        // if (response.ok && response.headers.get('content-type').includes('application/json')) {
-        if (response.ok) {
-            const data = await response.json();
-            res.json(data);
-        } else {
-            const rawText = await response.text();
-            console.log('Raw Response:', rawText);
-            res.status(500).json({error: 'Invalid response from API', rawText})
-        }
-    } catch (error) {
-        console.error('Error fetching API:', error.message);
-        res.status(500).json({ error: error.message});
+  const headers = generateAuthHeaders();
+
+  try {
+    const response = await fetch(
+      `${apiEndpoint}/episodes/byitunesid?id=${encodeURIComponent(feedId)}&max=${max}`,
+      {
+        method: "GET",
+        headers: headers,
+      },
+    );
+    // if (response.ok && response.headers.get('content-type').includes('application/json')) {
+    if (response.ok) {
+      const data = await response.json();
+      res.json(data);
+    } else {
+      const rawText = await response.text();
+      console.log("Raw Response:", rawText);
+      res.status(500).json({ error: "Invalid response from API", rawText });
     }
+  } catch (error) {
+    console.error("Error fetching API:", error.message);
+    res.status(500).json({ error: error.message });
+  }
 });
 
 app.listen(PORT, () => {
-//   console.log("Server is running on http://localhost:3000");
+  //   console.log("Server is running on http://localhost:3000");
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+// Git refresher practice
